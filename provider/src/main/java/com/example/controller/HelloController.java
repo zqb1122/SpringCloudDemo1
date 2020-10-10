54eb462456522f728d0828486d3a595fc7f6187e
zqb <zqb1122@outlook.com>
2020-10-10 17:13:32 +0800

    添加eureka和server

---
+++ provider/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        List<ServiceInstance> instances = client.getInstances("hello-service");
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "Hello World";
+    }
+}
